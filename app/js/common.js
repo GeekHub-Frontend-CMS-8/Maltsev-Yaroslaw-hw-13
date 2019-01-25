79e30954a8f460b5196f6a68e4f65165b3b53a17
JarikProg <44119194+JarikProg@users.noreply.github.com>
2019-01-25 17:36:23 +0200

    Add time

--- app/js/common.js
+++ app/js/common.js
@@ -4,7 +4,10 @@ ajaxList = [],
 list = $('.todos__list'),
 findList = '.todos__list',
 doc = $(document),
-localStorageValue = localStorage.getItem("text");
+localStorageValue = localStorage.getItem("text"),
+checkTimer;
+
+checkTimer = setInterval(function(){dateCheck()}, 3000)
 
 function editedStyle (data) {
 	if (data == true) {
@@ -25,6 +28,7 @@ else {
 
 function createWindowOpen (button) { //Open window to create ToDo's
 	$(button).on('click', function(){
+		timeOut();
 		$('.todos__create textarea').val('');
 		$('.todos__create *').css('display', 'flex');
 	})
@@ -42,7 +46,7 @@ createWindowClose('#button__cancel');
 
 let indexForRender = 0;
 
-function isDone(data, valueIfFalse, valueIfTrue) {
+function  styleFunc(data, valueIfFalse, valueIfTrue) {
 	if (data == false){
 		return valueIfFalse
 	}				
@@ -56,8 +60,8 @@ function render() {
 	let i = 0;
 	$.each(todoDescList, function(indexDel, indexEdit) {
 			list.prepend(`
-				<li class="todos__item" style="${isDone(todoDescList[i].completed, "", "background: rgba(0, 128, 0, 0.6)")}"> 
-					<textarea class="todos__desc" readonly style="${isDone(todoDescList[i].completed, "", "text-decoration: line-through; background: transparent; color: #fff;")}">${todoDescList[i].title}</textarea>
+				<li class="todos__item" style="${ styleFunc(todoDescList[i].completed, styleFunc(todoDescList[i].nowTime, "", "background: red"), "background: rgba(0, 128, 0, 0.6)")}"> 
+					<textarea class="todos__desc" readonly style="${ styleFunc(todoDescList[i].completed, "", "text-decoration: line-through; background: transparent; color: #fff;")}">${todoDescList[i].title}</textarea>
 					<button class="button button-item button__edit" data-index="${indexEdit}">
 						Edit
 					</button>
@@ -65,9 +69,10 @@ function render() {
 						Del
 					</button>
 					<button class="button button-item button__done">
-						${isDone(todoDescList[i].completed, "Done", "Not Done")}
+						${ styleFunc(todoDescList[i].completed, "Done", "Not Done")}
 					</button>
-					<p class="edited" style="width: 100%; text-align: left; display: ${editedStyle(todoDescList[i].edited)}; ${isDone(todoDescList[i].completed, "", "color: #fff")}">edited</p>
+					<input type="datetime-local" name="" class="time-input" value="${todoDescList[i].time}" style="border: 0" readonly>
+					<p class="edited" style="width: 100%; padding-left: 15px; font-size: 12px; text-align: left; display: ${editedStyle(todoDescList[i].edited)}; ${ styleFunc(todoDescList[i].completed, "", "color: #fff")}">edited</p>
 				</li>`)
 			i++;
 	});
@@ -83,7 +88,8 @@ function addToLocal() {
 */
 
 $('#create-new').on('click', function(){ //Create new ToDo's
-	let	text = $('#todoDescNew');
+	let	text = $('#todoDescNew'),
+			time = $('.time');
 	list.empty();
 	if (text[0].value == '') {
 		text.attr('placeholder','String is empty!').css('border', '1px solid red')
@@ -94,13 +100,18 @@ $('#create-new').on('click', function(){ //Create new ToDo's
 		todoDescList.push({
 				title: text[0].value,
 				completed: false,
-				edited: false
+				edited: false,
+				time: time.val(),
+				nowTime: false
 			}); //Add value to list
 
 		text.val(''); // Clear
+		time.val('');
 		addToLocal()	
 	}
+	dateCheck();
 	render();
+	checkTimer = setInterval(function(){dateCheck()}, 3000)
 })
 
 /*
@@ -110,29 +121,39 @@ $('#create-new').on('click', function(){ //Create new ToDo's
 let edit = false;
 
 doc.on('click', '.todos__item .button__edit', function(){ //Edit function
+	timeOut();
 	if (edit == false){
 		$(this).html('Save');
-		$(this).siblings('.todos__desc').css('border', '1px solid #ededed').removeAttr('readonly'); //Teaxtare view
+		$(this).siblings('.todos__desc').css('border', '1px solid #ededed').removeAttr('readonly');
+		$(this).siblings('.time-input').css('border', '1px solid #ededed').removeAttr('readonly');
 		edit = true;
 	}
 	else if (edit == true){
 		let index = $(this).siblings('.button__del').data('index'),
-				value = $(this).siblings('.todos__desc').val();
+				desc = $(this).siblings('.todos__desc').val(),
+				time = $(this).siblings('.time-input').val();
 
 		$(this).html('Edit');
 
-		todoDescList[index].title = value;
+		todoDescList[index].title = desc;
+
+		todoDescList[index].time = time;
 
 		// todoDescList[index].completed = false; - reset "done" when saving changes
 
 		todoDescList[index].edited = true;
 
+		edit = false;
+
+		$(this).siblings('.todos__desc').css('border', '0').attr('readonly');
+		$(this).siblings('.time-input').css('border', '0').attr('readonly');
+
+		dateCheck();
+
 		render();
 
 		addToLocal();
-
-		edit = false;
-		$(this).siblings('.todos__desc').css('border', '0').attr('readonly');
+		checkTimer = setInterval(function(){dateCheck()}, 3000)
 	};
 });
 
@@ -215,3 +236,29 @@ $('.clear').on('click', function() {
 	localStorage.clear();
 	render();
 })
+
+function timeNow() {
+	return new Date();
+};
+
+
+function dateCheck() {
+	let i, date;
+	for (i = 0; i < todoDescList.length; i++){
+
+		date = new Date(todoDescList[i].time);
+
+		if (date < timeNow() || date == timeNow()) {
+			todoDescList[i].nowTime = true;
+		}
+		else {
+			todoDescList[i].nowTime = false;
+		}		
+		render();
+		addToLocal();		
+	}	
+};
+function timeOut() {
+	clearInterval(checkTimer);
+	alert('stop')	
+};
